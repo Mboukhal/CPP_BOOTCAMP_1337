9c9fdd32b0216cfbbe3bb75fa06844c28b17b323
mboukhal <lios80466@gmail.com>
2022-12-07 13:47:28 +0100

    news

--- CppModule06/ex00/main.cpp
+++ CppModule06/ex00/main.cpp
@@ -1,87 +1,125 @@
-#include "Conversion.hpp"
-#include <stdlib.h>
+#include <iostream>
+#include <iostream>
+#include <string>
 #include <sstream>
+#include <iomanip>
 
-// #define SSTR( x ) static_cast< std::ostringstream & >( \
-//         ( std::ostringstream() << std::dec << x ) ).str()
-
-std::string toStr( int x ) {
-	std::ostringstream oss;
-	oss << x;
-	return oss.str();
+bool cheakIsValid( std::string str )
+{
+	for ( size_t i = 0; i < str.length(); i++ ) {
+		if ( i == str.length() - 1 && str[i] == 'f' )
+			break;
+		if ( !isdigit(str[i]) && str[i] != '.' && str[i] != '-')
+			return false;
+	}
+	return true;
 }
 
-std::string charRes( std::string in ) {
-	int tmp = atoi( in.c_str() );
-	std::string res = "\'";
-	std::string num ( 1, tmp );
-	res += num + '\'';
-	if ( tmp > 127 || tmp < 0 )
-		res = "Non displayable";
-	return res;
-}
-
-std::string intRes( std::string in ) {
+std::string floatPresantation( std::string in ) {
 	std::string res;
-	int tmp = atoi( in.c_str() );
-	res = toStr( tmp );
+	if ( in[in.length() - 1] == 'f' )
+		in.erase( in.length() - 1, 1 );
+	else
+		return res;
+	res = in;
 	return res;
 }
 
-std::string floatRes( std::string in ) {
-	std::string res;
-	float tmp = atof( in.c_str() );
-	res = toStr( tmp ) + ".0f";
-	return res;
-}
-
-std::string doubleRes( std::string in ) {
-	std::string res;
-	double tmp = std::stod( in.c_str() );
-	res = toStr( tmp );
-	return res;
-}
-
-void start( std::string& input ){
-	// if ( input )
-
-	std::string res = "char: ";
-	res += charRes( input ) + '\n';
-	res += "int: ";
-	res += intRes( input ) + '\n';
-	res += "float: ";
-	res += floatRes( input ) + '\n';
-	res += "double: ";
-	res += doubleRes( input ) + '\n';
-	std::cout << res;
-}
-
-// 	if ( input.size() == 1 ) {
-// 		toChar( input );
-// 		// return;
-// 	}
-// 	if ( input.find("f") )
-// 	std::cout << input << '\n';
-//     // convert string to float
-//     float num_float = std::stof(input);
-// 	std::cout << input << ": " << num_float << '\n';
-
-// //     // convert string to double
-//     double num_double = std::stod(input);
-// 	std::cout << input << ": " << num_double << '\n';
-
-// //     // convert string to int
-//     int num_int = std::stoi(input);
-// 	std::cout << input << ": " << num_int << '\n';
-
-int main ( int ac, char **av ) {
-
-	if ( ac == 2 )
+int main( int ac, char** av )
+{
+	if (ac == 2)
 	{
-		std::string input = std::string(av[1]);
-		start( input );
+		{
+			std::cout << "Char: ";
+			int a;
+			std::stringstream stream;
+			stream << av[1];
+			stream >> a;
+			if ( !cheakIsValid( av[1] ) )
+				std::cout << "impossible" << std::endl;
+			else {
+				if ( isprint(a) )
+					std::cout << "\'" << static_cast<char>(a) << "\'" << std::endl;
+				else
+					std::cout << "Non displayable" << std::endl;
+			}
+		}
+
+		{
+			std::cout << "Int: ";
+			int a;
+
+			std::string x = av[1];
+			if ( x == "+inff" || x == "-inf" )
+					std::cout << "impossible" << std::endl;
+			else {
+				std::stringstream stream;
+				stream << av[1];
+				stream >> a;
+				if ( !cheakIsValid( av[1] ) )
+					std::cout << "impossible" << std::endl;
+				else
+					std::cout << a << std::endl;
+			}
+		}
+
+		{
+			std::cout << "Float: ";
+			std::string x = av[1];
+			if ( x == "+inff")
+				std::cout << "+inf" << std::endl;
+			else if ( x == "-inf" )
+				std::cout << "-inff" << std::endl;
+			else {
+				float a;
+				x = "";
+				x = floatPresantation( av[1] );
+				std::stringstream stream;
+				if ( x.empty() )
+					stream << av[1];
+				else
+					stream << x;
+				stream >> a; 
+				if ( !cheakIsValid( av[1] ) )
+					std::cout << "nanf" << std::endl;
+				else
+				{
+					std::cout << std::fixed << std::setprecision(1)
+							  << static_cast<float>(a) << "f" << std::endl;
+				}
+			}
+		}
+
+		{
+			std::cout << "Double: ";
+			std::string x ( av[1] );
+			if ( x == "+inf")
+				std::cout << "+inf" << std::endl;
+			else if ( x == "-inf" )
+				std::cout << "-inf" << std::endl;
+			else {
+				double a;
+				x = "";
+				x = floatPresantation( av[1] );
+				std::stringstream stream;
+				if ( x.empty() )
+					stream << av[1];
+				else
+					stream << x;
+				stream >> a; 
+				if ( !cheakIsValid( av[1] ) )
+					std::cout << "nan" << std::endl;
+				else
+				{
+					std::cout << std::fixed << std::setprecision(1)
+							  << static_cast<double>(a) << std::endl;
+				}
+			}
+		}
 		return 0;
 	}
+	else
+		std::cerr << "Error!" << std::endl;
 
-	return 1;
+    return 1;
 }
--- CppModule06/ex01/main.cpp
+++ CppModule06/ex01/main.cpp
@@ -1,6 +1,18 @@
-
+#include "serial.hpp"
 
 int main ( void ) {
 
+	Data info = { 'c', "hi World!", 42 };
+
+	std::cout << info << std::endl;
+	uintptr_t serialData = serialize( &info );
+
+	std::cout << (char *)serialData << std::endl;
+
+	Data *reciver = deserialize( serialData );
+	std::cout << *reciver << std::endl;
+
+
+	
 	return 0;
 }
